a14f1e7e15265a3c2b25fcb2dbd03313034cf6e6
Thomas <thomas.cornillet@gmail.com>
2017-05-01 15:59:22 +0200

    Création automatique des fichiers ini à partir des csv

--- saisons/csvtoini.py
+++ saisons/csvtoini.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 # -*-coding:Utf-8 -*
 
-import os
-import csv
-import configparser
-
 """
 
 Script pour créer automatiquement les ini à partir des csv
@@ -12,102 +8,86 @@ Script pour créer automatiquement les ini à partir des csv
 Premier test 	: créer manuellement test.ini 				: OK
 Deuxième test	: créer manuellement 20.ini 				: OK
 Troisième test	: remplir automatiquement 20.ini 			: NOK
-Quatrième test 	: créer et remplire automatiquement 20.ini	:
+Quatrième test 	: créer et remplire automatiquement 20.ini	: OK
 
-Il faudrait modifier les csv en remplaçant le sépérateur de ',' à ";"
+Attention, problème avec l'épisode "1%" de le saison 15, le '%' faisant planter configparser
+	du coup, il faut l'enlever et l'ajouter manuellement
+	sinon, ça fonctionne
 
 """
+
+import os
+import csv
+import configparser
 
 path = "/home/thomas/Data/Python/South Park/saisons"
 os.chdir(path)
 
-# on initialise un fichier ini
-fichier_ini = configparser.ConfigParser()
+a = 1
+nb_saisons = 20
 
-# on charge le fichier csv
-saison_csv = ""
-with open ("20.csv","r") as fichier_csv:
-	fichier_reader = csv.reader(fichier_csv)
-	for row in fichier_reader:
-		saison_csv = saison_csv + str(row)
+while a <= nb_saisons:
 
-# on crée la première clef
-fichier_ini['saison'] = {}
-fichier_ini['saison']['num_saison'] = ""
-fichier_ini['saison']['nb_episodes'] = ""
-fichier_ini['saison']['chemin'] = ""
+	# on initialise un fichier ini
+	fichier_ini = configparser.ConfigParser()
 
-# on crée les autres clefs
-fichier_ini['episode_1'] = {}
-fichier_ini['episode_1']['nom_episode'] = ""
-fichier_ini['episode_1']['num_episode'] = ""
-fichier_ini['episode_1']['nb_vues'] = ""
-fichier_ini['episode_1']['derniere_vue'] = ""
-fichier_ini['episode_1']['double'] = ""
+	# on charge le fichier csv
+	nom_csv = a
+	if a < 10:
+		nom_csv = str(nom_csv)
+		nom_csv = '0' + nom_csv + ".csv"
+	else:
+		nom_csv = str(nom_csv)
+		nom_csv = nom_csv + ".csv"
+	
+	saison_csv = ""
+	with open (nom_csv,"r") as fichier_csv:
+		fichier_reader = csv.reader(fichier_csv, delimiter=';')
+		for row in fichier_reader:
+			ligne = row
+			ligne = ";".join(ligne)
+			saison_csv = saison_csv + "/" + ligne
+	saison_csv = saison_csv.split("/")
+	del saison_csv[0]
+	del saison_csv[0]
+	fichier_csv.close()
+	
+	# ici, saison_csv est une liste comportant toutes les informations sur les épisodes de la saison, les inforamtions étant séparées par des ';'
+	
+	# on crée la première clef
+	fichier_ini['saison'] = {}
+	fichier_ini['saison']['num_saison'] = str(a)
+	nb_episodes = len(saison_csv)
+	fichier_ini['saison']['nb_episodes'] = str(nb_episodes)
+	fichier_ini['saison']['chemin'] = "" # à emplir à la main
 
-fichier_ini['episode_2'] = {}
-fichier_ini['episode_2']['nom_episode'] = ""
-fichier_ini['episode_2']['num_episode'] = ""
-fichier_ini['episode_2']['nb_vues'] = ""
-fichier_ini['episode_2']['derniere_vue'] = ""
-fichier_ini['episode_2']['double'] = ""
+	i = 0
+	while i < nb_episodes:
+		# on charge l'épisode
+		ligne_ep = saison_csv[i]
+		ligne_ep = ligne_ep.split(";")
+		# on crée le clé épisode dans le fichier ini
+		num_ep = "episode_"
+		num = ligne_ep[0]
+		num_ep = num_ep + str(num)
+		fichier_ini[num_ep] = {}
+		fichier_ini[num_ep]['nom_episode'] = ligne_ep[1]
+		fichier_ini[num_ep]['nb_vues'] = str(ligne_ep[4])
+		fichier_ini[num_ep]['derniere_vue'] = ""
+		fichier_ini[num_ep]['double'] = ligne_ep[3]
+		fichier_ini[num_ep]['chemin'] = ""
+		i += 1
 
-fichier_ini['episode_3'] = {}
-fichier_ini['episode_3']['nom_episode'] = ""
-fichier_ini['episode_3']['num_episode'] = ""
-fichier_ini['episode_3']['nb_vues'] = ""
-fichier_ini['episode_3']['derniere_vue'] = ""
-fichier_ini['episode_3']['double'] = ""
-
-fichier_ini['episode_4'] = {}
-fichier_ini['episode_4']['nom_episode'] = ""
-fichier_ini['episode_4']['num_episode'] = ""
-fichier_ini['episode_4']['nb_vues'] = ""
-fichier_ini['episode_4']['derniere_vue'] = ""
-fichier_ini['episode_4']['double'] = ""
-
-fichier_ini['episode_5'] = {}
-fichier_ini['episode_5']['nom_episode'] = ""
-fichier_ini['episode_5']['num_episode'] = ""
-fichier_ini['episode_5']['nb_vues'] = ""
-fichier_ini['episode_5']['derniere_vue'] = ""
-fichier_ini['episode_5']['double'] = ""
-
-fichier_ini['episode_6'] = {}
-fichier_ini['episode_6']['nom_episode'] = ""
-fichier_ini['episode_6']['num_episode'] = ""
-fichier_ini['episode_6']['nb_vues'] = ""
-fichier_ini['episode_6']['derniere_vue'] = ""
-fichier_ini['episode_6']['double'] = ""
-
-fichier_ini['episode_7'] = {}
-fichier_ini['episode_7']['nom_episode'] = ""
-fichier_ini['episode_7']['num_episode'] = ""
-fichier_ini['episode_7']['nb_vues'] = ""
-fichier_ini['episode_7']['derniere_vue'] = ""
-fichier_ini['episode_7']['double'] = ""
-
-fichier_ini['episode_8'] = {}
-fichier_ini['episode_8']['nom_episode'] = ""
-fichier_ini['episode_8']['num_episode'] = ""
-fichier_ini['episode_8']['nb_vues'] = ""
-fichier_ini['episode_8']['derniere_vue'] = ""
-fichier_ini['episode_8']['double'] = ""
-
-fichier_ini['episode_9'] = {}
-fichier_ini['episode_9']['nom_episode'] = ""
-fichier_ini['episode_9']['num_episode'] = ""
-fichier_ini['episode_9']['nb_vues'] = ""
-fichier_ini['episode_9']['derniere_vue'] = ""
-fichier_ini['episode_9']['double'] = ""
-
-fichier_ini['episode_10'] = {}
-fichier_ini['episode_10']['nom_episode'] = ""
-fichier_ini['episode_10']['num_episode'] = ""
-fichier_ini['episode_10']['nb_vues'] = ""
-fichier_ini['episode_10']['derniere_vue'] = ""
-fichier_ini['episode_10']['double'] = ""
-
-# on écrit le fichier
-with open('20.ini','w') as saison:
-	fichier_ini.write(saison)
+	# on écrit le fichier
+	nom_ini = a
+	if a <10:
+		nom_ini = str(nom_ini)
+		nom_ini = '0' + nom_ini + ".ini"
+	else:
+		nom_ini = str(nom_ini)
+		nom_ini = nom_ini + ".ini"
+	with open(nom_ini,'w') as saison:
+		fichier_ini.write(saison)
+	saison.close()
+	
+	a += 1
