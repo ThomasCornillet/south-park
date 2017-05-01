a06d587024e7a917b7572ed4c1f7719865395b05
Thomas <thomas.cornillet@gmail.com>
2017-05-01 18:55:13 +0200

    Plus de csv, que des ini

--- south_park.py
+++ south_park.py
@@ -3,26 +3,24 @@
 
 """
 
-Initialisation de la v2.1 sur la branche master
-	- passage des fichiers csv à des fichiers ini
-		pour le fait avec un script, je vais d'abord modifier les spéarateurs des csv, passant de "," à ";"
-			ce qui reglera le bug
+south_park.py v2.1 sur branche master
 
-
-Bugs en cours : 
-	- si les noms d'épisodes contiennent des virgules ','
-		mauvais split pour ligne_episode
+Bugs connus : 
 
 """
 
-import saisons # je ne suis pas sur que ça serve à quelque chose... (en tout cas, ça n'empêche pas de devoir faire un os.chdir() pour rentrer dans le dossier saisons
+import saisons # je ne suis pas sur que ça serve à quelque chose
 import os
 import csv
+import configparser
 from random import randint
 from subprocess import Popen # je ne m'en sers pas encore, c'est pour l'ouverture automatique du fichier
 
 print("\nBienvenue dans la sélection aléatoire d'un épisode de South Park\n")
 print("L'application va définir aléatoirement un épisode.\n")
+
+path = "/home/thomas/Data/Python/South Park/saisons"
+os.chdir(path)
 
 reponse = 'n'
 while reponse == 'n':
@@ -32,72 +30,82 @@ while reponse == 'n':
 		saison = '0' + saison
 	else:
 		saison = saison
-	saison = saison + ".csv"
-	path = "/home/thomas/Data/Python/South Park/saisons"
-	os.chdir(path)
-	fichier = open(saison, "r") # penser à os.chdir(./saisons), mais le "./" n'est pas reconnu pas Python...
-	ligne = fichier.readlines()
-	fichier.close()
-	nb_episodes = len(ligne) - 1
-	episode = randint(1,nb_episodes)
-	ligne_episode = str(ligne[episode])
-	ligne_episode = ligne_episode.split(",")
-	nom_episode = ligne_episode[1] 
-
-	print("L'application a sélectionné l'épisode {}, n°{}/{}, de la saison {}\n".format(nom_episode,episode,nb_episodes,season))
+	saison = saison + ".ini"
+	saison_ini = configparser.ConfigParser()
+	saison_ini.read(saison)
+	nb_episodes = saison_ini['saison']['nb_episodes']
+	nb_episodes = int(nb_episodes)
+	num_episode = randint(1,nb_episodes)
+	if num_episode < 10:
+		chemin_episode = str(num_episode)
+		chemin_episode = '0' + chemin_episode
+	else:
+		chemin_episode = str(num_episode)
+	chemin_episode = "episode_" + chemin_episode
+	nom_episode = saison_ini[chemin_episode]['nom_episode']
+	nb_vues = saison_ini[chemin_episode]['nb_vues']
+	nb_vues = int(nb_vues)
 	
-	nb_vues = int(ligne_episode[4])
+	print("L'application a sélectionné l'épisode {}, n°{}/{}, de la saison {}\n".format(nom_episode,num_episode,nb_episodes,season))
+	
 	if nb_vues == 0:
 		print("Vous n'avez pas encore vu cet épisode.\n")
 	else:
 		print("Vous avez déjà vu cet épisode {} fois\n".format(nb_vues))
-	
+
 	# on veut tester maintenant si l'épisode se trouve dans un arc narratif
-	
-	# on définit les valeurs de deux épisodes antérieurs et deux deux d'après
-	# mais on ne les charge pas encore, au cas où il y ait un IndexError
-	ep_ant = episode - 1
-	ep_ant2 = ep_ant - 1
-	ep_apr = episode + 1
-	ep_apr2 = episode + 2
-	
 	# on charge la variable double de l'épisode sélectionné
-	double = ligne_episode[3]
-	
+	double = saison_ini[chemin_episode]['double']
+	# on charge le n° des épisodes de check de la place dans l'arc
+	ep_ant = num_episode - 1
+	ep_ant2 = num_episode - 2
+	ep_apr = num_episode + 1
+	ep_apr2 = num_episode + 2
+	if num_episode < 8:
+		chemin_ant = "episode_0" + str(ep_ant)
+		chemin_ant2 = "episode_0" + str(ep_ant2)
+		chemin_apr = "episode_0" + str(ep_apr)
+		chemin_apr2 = "episode_0" + str(ep_apr2)
+	elif num_episode == 8:
+		chemin_ant = "episode_0" + str(ep_ant)
+		chemin_ant2 = "episode_0" + str(ep_ant2)
+		chemin_apr = "episode_0" + str(ep_apr)
+		chemin_apr2 = "episode_" + str(ep_apr2)
+	elif num_episode == 9:
+		chemin_ant = "episode_0" + str(ep_ant)
+		chemin_ant2 = "episode_0" + str(ep_ant2)
+		chemin_apr = "episode_" + str(ep_apr)
+		chemin_apr2 = "episode_" + str(ep_apr2)
+	elif num_episode == 10:
+		chemin_ant = "episode_0" + str(ep_ant)
+		chemin_ant2 = "episode_0" + str(ep_ant2)
+		chemin_apr = "episode_" + str(ep_apr)
+		chemin_apr2 = "episode_" + str(ep_apr2)
+	elif num_episode == 11:
+		chemin_ant = "episode_" + str(ep_ant)
+		chemin_ant2 = "episode_0" + str(ep_ant2)
+		chemin_apr = "episode_" + str(ep_apr)
+		chemin_apr2 = "episode_" + str(ep_apr2)
+	else:
+		chemin_ant = "episode_" + str(ep_ant)
+		chemin_ant2 = "episode_" + str(ep_ant2)
+		chemin_apr = "episode_" + str(ep_apr)
+		chemin_apr2 = "episode_" + str(ep_apr2)
+
 	if double == "True":
 		print("Attention, l'épisode sélectionné fait partie d'un arc narratif")
-
-		if episode == 1:
-			# c'est le premier de la saison
-			# on charge donc l'épisode d'après
-			ligne_ep_apr = str(ligne[ep_apr])
-			ligne_ep_apr = ligne_ep_apr.split(",")
-			double_apr = ligne_ep_apr[3]
-			# et l'épisode encore d'après
-			ligne_ep_apr2 = str(ligne[ep_apr2])
-			ligne_ep_apr2 = ligne_ep_apr2.split(",")
-			double_apr2 = ligne_ep_apr2[3]
-			
+		
+		if num_episode == 1:
+			double_apr = saison_ini[chemin_apr]['double']
+			double_apr2 = saison_ini[chemin_apr2]['double']
 			if double_apr2 == "True":
 				print("Il s'agit du premier épisode d'une \"trilogie\".\n")
 			else:
 				print("Il s'agit de la première partie d'un double épisode\n")
-				
-		elif episode == 2:
-			# c'est le deuxième de la saison
-			# on charge l'épisode antérieur
-			ligne_ep_ant = str(ligne[ep_ant])
-			ligne_ep_ant = ligne_ep_ant.split(",")
-			double_ant = ligne_ep_ant[3]
-			# on charge donc l'épisode d'après
-			ligne_ep_apr = str(ligne[ep_apr])
-			ligne_ep_apr = ligne_ep_apr.split(",")
-			double_apr = ligne_ep_apr[3]
-			# et l'épisode encore d'après
-			ligne_ep_apr2 = str(ligne[ep_apr2])
-			ligne_ep_apr2 = ligne_ep_apr2.split(",")
-			double_apr2 = ligne_ep_apr2[3]
-			
+		elif num_episode == 2:
+			double_ant = saison_ini[chemin_ant]['double']
+			double_apr = saison_ini[chemin_apr]['double']
+			double_apr2 = saison_ini[chemin_apr2]['double']
 			if double_ant == "True":
 				if double_apr == "True":
 					print("Il s'agit du deuxième épisode d'une \"trilogie\".\n")
@@ -108,38 +116,19 @@ while reponse == 'n':
 					print("Il s'agit du premier épisode d'une \"trilogie\".\n")
 				else:
 					print("Il s'agit de la première partie d'un double épisode\n")
-	
-		elif episode == nb_episodes:
-			# c'est le dernier de la saison
-			# on charge l'épisode antérieur
-			ligne_ep_ant = str(ligne[ep_ant]) 
-			ligne_ep_ant = ligne_ep_ant.split(",")
-			double_ant = ligne_ep_ant[3]
-			# on charge l'épisode antérieur à l'épisode antérieur
-			ligne_ep_ant2 = str(ligne[ep_ant2])
-			ligne_ep_ant2 = ligne_ep_ant2.split(",")
-			double_ant2 = ligne_ep_ant2[3]
-			
+		
+		elif num_episode == nb_episodes:
+			double_ant = saison_ini[chemin_ant]['double']
+			double_ant2 = saison_ini[chemin_ant2]['double']
 			if double_ant2 == "True":
 				print("Il s'agit du dernier épisode d'une \"trilogie\".\n")
 			else:
 				print("Il s'agit de la deuxième partie d'un double épisode\n")
 		
-		elif episode == nb_episodes - 1:
-			# c'est l'avant dernier de la saison
-			# on charge l'épisode antérieur
-			ligne_ep_ant = str(ligne[ep_ant])
-			ligne_ep_ant = ligne_ep_ant.split(",")
-			double_ant = ligne_ep_ant[3]
-			# on charge l'épisode antérieur à l'épisode antérieur
-			ligne_ep_ant2 = str(ligne[ep_ant2])
-			ligne_ep_ant2 = ligne_ep_ant2.split(",")
-			double_ant2 = ligne_ep_ant2[3]
-			# on charge donc l'épisode d'après
-			ligne_ep_apr = str(ligne[ep_apr])
-			ligne_ep_apr = ligne_ep_apr.split(",")
-			double_apr = ligne_ep_apr[3]
-			
+		elif num_episode == nb_episodes - 1:
+			double_ant = saison_ini[chemin_ant]['double']
+			double_ant2 = saison_ini[chemin_ant2]['double']
+			double_apr = saison_ini[chemin_apr]['double']
 			if double_apr == "True":
 				if double_ant == "True":
 					print("Il s'agit du deuxième épisode d'une \"trilogie\".\n")
@@ -150,36 +139,13 @@ while reponse == 'n':
 					print("Il s'agit du dernier épisode d'une \"trilogie\".\n")
 				else:
 					print("Il s'agit de la deuxième partie d'un double épisode\n")
-		
+			
 		else:
-			# on charge l'épisode antérieur
-			ligne_ep_ant = str(ligne[ep_ant]) 
-			ligne_ep_ant = ligne_ep_ant.split(",")
-			double_ant = ligne_ep_ant[3]
-			# on charge l'épisode antérieur à l'épisode antérieur
-			ligne_ep_ant2 = str(ligne[ep_ant2])
-			ligne_ep_ant2 = ligne_ep_ant2.split(",")
-			double_ant2 = ligne_ep_ant2[3]
-			# on charge donc l'épisode d'après
-			ligne_ep_apr = str(ligne[ep_apr])
-			ligne_ep_apr = ligne_ep_apr.split(",")
-			double_apr = ligne_ep_apr[3]
-			# et l'épisode encore d'après
-			ligne_ep_apr2 = str(ligne[ep_apr2])
-			ligne_ep_apr2 = ligne_ep_apr2.split(",")
-			double_apr2 = ligne_ep_apr2[3]
-	
-			if double_ant2 == "True":
-				print("Il s'agit du dernier épisode d'une \"trilogie\".\n")
-			elif double_apr2 == "True":
-				print("Il s'agit du premier épisode d'une \"trilogie\".\n")
-			elif double_ant == "True" and double_apr == "True":
-				print("Il s'agit du deuxième épisode d'une \"trilogie\".\n")
-			elif double_ant == "False" and double_apr2 == "False":
-				print("Il s'agit de la première partie d'un double épisode\n")
-			elif double_apr == "False" and double_ant2 == "False":
-				print("Il s'agit de la deuxième partie d'un double épisode\n")
-
+			double_ant = saison_ini[chemin_ant]['double']
+			double_ant2 = saison_ini[chemin_ant2]['double']
+			double_apr = saison_ini[chemin_apr]['double']
+			double_apr2 = saison_ini[chemin_apr2]['double']
+			
 	reponse = input("Cela vous convient-il ? o/n\n")
 	reponse = reponse.lower()
 	while reponse != 'o' and reponse != 'n':
